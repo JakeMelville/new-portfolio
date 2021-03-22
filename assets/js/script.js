d80b0aa51b2c0bdde5249c9ea801959d26e4287e
Jake Melville <jakemelville@ymail.com>
2021-03-22 19:46:27 -0400

    started working on js file- now have contact me and about me hidden/visible depending on click. Edited my contact info and about me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 const appBtn = document.getElementById("appBtn");
 const resumeBtn = document.getElementById("resumeBtn");
 
-resumeBtn.addEventListener('click', function() {
+
+
+// resumeBtn.addEventListener('click', function() {
     
-}) 
+// }) 
+
+
+function showAboutMe(event) {
+    document.getElementById("contactCard").setAttribute("hidden", true)
+    document.getElementById("aboutCard").removeAttribute('hidden');
+    console.log('clicked')
+}
+function showContactCard(event) {
+    document.getElementById("aboutCard").setAttribute('hidden', true);
+    document.getElementById("contactCard").removeAttribute('hidden')
+}
+document.getElementById("abtMe").addEventListener("click", showAboutMe)
+document.getElementById("meContact").addEventListener("click", showContactCard)
